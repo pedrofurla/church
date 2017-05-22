8219053ff49e6595becd61d8ac3838ed42b9ec28
Pedro Furlanetto <pedrofurla@gmail.com>
2017-05-21 23:06:40 -0400

    Haskell version

---
+++ src/main/haskell/logic.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Prelude hiding (Bool, and)
+
+type Bool a = a -> a -> a
+
+true2 ::  Bool a
+true2 a b = a
+
+false2 ::  Bool a
+false2 a b = b
+	
+--and2 :: Bool a -> Bool a -> Bool a
+and2 l r a b = l (r a b) (l a b)
+
+or2 l r a b  = l (l a b) (r a b)
+
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
